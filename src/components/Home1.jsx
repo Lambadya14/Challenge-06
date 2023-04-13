026b5cb946e291e2e465c0d9e25875718f84d7f5
Daniel <devilwhitedemon@gmail.com>
2023-04-13 15:47:50 +0700

    update: display adjusment

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -47,48 +47,67 @@ function Home1() {
           <NavbarPage />
         </Col>
       </Row>
-      <Slider {...settings}>
-        {movies.map((image, index) => (
-          <div key={index}>
-            <div
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                display: "grid",
-                placeItems: "bottom",
-                width: "100%",
-                height: "600px",
-                marginBottom: "5px",
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0)100%),url(https://image.tmdb.org/t/p/original/${image.backdrop_path})`,
-              }}
-            >
-              <span
-                onClick={() => {
-                  navigate(`/movie/${image.id}`);
-                }}
-                style={{
-                  spanStyle,
-                  color: "White",
-                  marginTop: "200px",
-                  marginLeft: "15px",
-                  marginRight: "700px",
-                }}
-              >
-                <h1 style={{ fontSize: "80px" }}>{image.title}</h1>
-                <p>{image.overview}</p>
-                <Button className="bg-danger border-0 rounded-5">
-                  <h6 className="mx-1 my-2">
-                    <MdMovie /> Watch Trailer
-                  </h6>
-                </Button>
-              </span>
-            </div>
-          </div>
-        ))}
-      </Slider>
+      <Row>
+        <Col>
+          <Slider {...settings}>
+            {movies.map((image, index) => (
+              <div key={index}>
+                <div
+                  style={{
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    display: "grid",
+                    placeItems: "bottom",
+                    width: "100%",
+                    height: "600px",
+                    marginBottom: "5px",
+                    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5) 100%, rgba(0, 0, 0, 0)100%),url(https://image.tmdb.org/t/p/original/${image.backdrop_path})`,
+                  }}
+                >
+                  <span
+                    onClick={() => {
+                      navigate(`/movie/${image.id}`);
+                    }}
+                    style={{
+                      spanStyle,
+                      color: "White",
+                      marginTop: "150px",
+                      marginLeft: "15px",
+                      marginRight: "700px",
+                      marginBottom: "100px",
+                    }}
+                    className="mx-5"
+                  >
+                    <Row>
+                      <Col className="col-md-8 ">
+                        <h1 style={{ fontSize: "80px" }} className="my-2">
+                          {image.title}
+                        </h1>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col className="col-md-6 ">
+                        <p className="my-2">{image.overview}</p>
+                      </Col>
+                    </Row>
+
+                    <Row>
+                      <Col className="col-md-6">
+                        <Button className="bg-danger border-0 rounded-5 my-2">
+                          <h6 className="mx-1 my-2">
+                            <MdMovie /> Watch Trailer
+                          </h6>
+                        </Button>
+                      </Col>
+                    </Row>
+                  </span>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </Col>
+      </Row>
     </>
   );
 }
--- src/components/Home2.jsx
+++ src/components/Home2.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import axios from "axios";
-import { useEffect, useState, useNavigate } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Row, Col, Container, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { motion } from "framer-motion";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { MdStarOutline } from "react-icons/md";
 
 function Home2() {
   const [movies, setMovies] = useState([]);
@@ -13,7 +17,7 @@ function Home2() {
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
   };
 
   // const params = useParams();
@@ -32,47 +36,52 @@ function Home2() {
   }, []);
   return (
     <Container className="d-flex flex-column gap-2 ">
-      <Row>
+      <Row className="mt-5">
         <Col className="d-flex justify-content-between align-items-center">
           <h1>Popular Films</h1>
           <p>See More Populars!</p>
         </Col>
       </Row>
-      <Row>
+      <Row className="mb-5">
         <Col>
           <Slider {...settings}>
             {movies.map((image) => (
-              <Card className="border-0" style={{ width: "18rem" }}>
-                <Card.Body>
-                  <Card.Img
-                    variant="top"
-                    style={{ width: 286, height: 400 }}
-                    src={`https://image.tmdb.org/t/p/original/${image.poster_path}`}
-                  />
+              <Row>
+                <Col>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img
+                      variant="top"
+                      src={`https://image.tmdb.org/t/p/original/${image.poster_path}`}
+                      onClick={() => {
+                        navigate(`/movie/${image.id}`);
+                      }}
+                    />
 
-                  <Card.Title
-                    onClick={() => {
-                      navigate(`/movie/${image.id}`);
-                    }}
-                    style={{ fontSize: "15px" }}
-                  >
-                    {image.title}
-                  </Card.Title>
-                  <Card.Text
-                    onClick={() => {
-                      navigate(`/movie/${image.id}`);
-                    }}
-                  >
-                    <p>
-                      <MdStarOutline
-                        style={{ color: "yellow" }}
-                        className="mx-2"
-                      />
-                      {image.vote_average} / 10
-                    </p>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
+                    <Card.Title
+                      className="mx-3 mt-3"
+                      onClick={() => {
+                        navigate(`/movie/${image.id}`);
+                      }}
+                      style={{ fontSize: "15px" }}
+                    >
+                      {image.title}
+                    </Card.Title>
+                    <Card.Text
+                      onClick={() => {
+                        navigate(`/movie/${image.id}`);
+                      }}
+                    >
+                      <p>
+                        <MdStarOutline
+                          style={{ color: "yellow" }}
+                          className="mx-2"
+                        />
+                        {image.vote_average} / 10
+                      </p>
+                    </Card.Text>
+                  </Card>
+                </Col>
+              </Row>
             ))}
           </Slider>
         </Col>
